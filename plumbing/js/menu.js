880a7339ae15c5dc11ad13c16b2cecdbfc4eff9c
skliffy <63788708+skliffy@users.noreply.github.com>
2025-12-24 19:21:05 +0500

    Delete plumbing directory

--- plumbing/js/menu.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-  const mobileMenu = document.getElementById('mobileMenu');
-  mobileMenu.classList.add('active');
-}
-
-function closeMenu() {
-  const mobileMenu = document.getElementById('mobileMenu');
-  mobileMenu.classList.remove('active');
-}
