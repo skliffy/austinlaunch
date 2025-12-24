053e21a96fc07dbd9a07dacc174a7554e90ae24c
skliffy <63788708+skliffy@users.noreply.github.com>
2025-12-24 19:26:32 +0500

    Add files via upload

---
+++ plumbing/js/menu.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  const mobileMenu = document.getElementById('mobileMenu');
+  mobileMenu.classList.add('active');
+}
+
+function closeMenu() {
+  const mobileMenu = document.getElementById('mobileMenu');
+  mobileMenu.classList.remove('active');
+}
